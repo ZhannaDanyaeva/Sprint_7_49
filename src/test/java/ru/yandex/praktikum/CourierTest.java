b4325d3c677a4ba5a747544ffbd224b876638e33
ZhannaDan <zh.i.danyaeva@gmail.com>
2025-09-21 19:34:06 +0300

    fix

--- src/test/java/ru/yandex/praktikum/CourierTest.java
+++ src/test/java/ru/yandex/praktikum/CourierTest.java
@@ -1,134 +1,156 @@
-import io.qameta.allure.Description;
-import io.restassured.response.ValidatableResponse;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
+package ru.yandex.praktikum;
 
-import java.time.LocalDate;
+import io.qameta.allure.Description;
+import io.restassured.RestAssured;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.*;
+
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
-import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class CourierTest {
 
-    private final String baseUri = "https://qa-scooter.praktikum-services.ru/";
-
     private int courierId;
+    private String baseUri = "https://qa-scooter.praktikum-services.ru";  // Используем правильную базовую ссылку
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = baseUri;
+    }
+
+    @Test
+    @Order(1)
+    @Description("Проверка создания курьера и авторизация")
+    public void testCreateCourier() {
+        String currentTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmssSSS"));
+
+        String courierLogin = "login" + currentTime;
+
+        String courierJson = "{ \"login\": \"" + courierLogin + "\", \"password\": \"12345\", \"firstName\": \"TestUser\" }";
+
+        ValidatableResponse createResponse = given()
+                .header("Content-Type", "application/json")
+                .body(courierJson)
+                .when()
+                .post("api/v1/courier")
+                .then()
+                .statusCode(201)  // Ожидаем статус 201
+                .body("ok", equalTo(true));
+
+        courierId = createResponse.extract().path("id");
+
+        if (courierId == 0) {
+            throw new RuntimeException("Courier ID not found in the response.");
+        }
+
+        System.out.println("Created courier ID: " + courierId);
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(courierId)
+                .when()
+                .post("api/v1/courier/login")
+                .then()
+                .statusCode(200)
+                .body("id", notNullValue());  // Проверяем, что ID курьера возвращается
+
+
+    }
+
+    @Test
+    @Order(3)
+    @Description("Проверка удаления курьера")
+    public void testDeleteCourier() {
+        String currentTime = LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmssSSS"));
+
+        String courierLogin = "login" + currentTime;
+
+        String courierJson = "{ \"login\": \"" + courierLogin + "\", \"password\": \"12345\", \"firstName\": \"TestUser\" }";
+
+        ValidatableResponse createResponse = given()
+                .header("Content-Type", "application/json")
+                .body(courierJson)
+                .when()
+                .post("api/v1/courier")
+                .then()
+                .statusCode(201)  // Ожидаем статус 201
+                .body("ok", equalTo(true));
+
+        courierId = createResponse.extract().path("id");
+
+        if (courierId == 0) {
+            throw new RuntimeException("Courier ID not found. Please create a courier first.");
+        }
+
+        given()
+                .header("Content-Type", "application/json")
+                .when()
+                .delete("api/v1/courier/" + courierId)
+                .then()
+                .statusCode(200)
+                .body("ok", equalTo(true));
+
+        courierId = 0;
+    }
+
+    @Test
+    @Order(4)
+    @Description("Проверка создания двух одинаковых курьеров")
+    public void testCreateDuplicateCourier() {
+        // Создаем курьера с дублирующим логином
+        String loginJson = "{ \"login\": \"login" + LocalTime.now().format(DateTimeFormatter.ofPattern("HHmmssSSS")) + "\", \"password\": \"12345\" }";
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(loginJson)
+                .when()
+                .post("api/v1/courier")
+                .then()
+                .statusCode(201)
+                .body("ok", equalTo(true));
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(loginJson)
+                .when()
+                .post("api/v1/courier")
+                .then()
+                .statusCode(409)  // Конфликт — логин уже существует
+                .body("message", equalTo("Этот логин уже используется. Попробуйте другой."));
+    }
+
+    @Test
+    @Order(5)
+    @Description("Проверка создания курьера без обязательных полей")
+    public void testCreateCourierWithoutRequiredFields() {
+        String courierJson = "{ \"login\": \"\", \"password\": \"\", \"firstName\": \"\" }";
+
+        given()
+                .header("Content-Type", "application/json")
+                .body(courierJson)
+                .when()
+                .post("api/v1/courier")
+                .then()
+                .statusCode(400)  // Ожидаем ошибку 400, если поля пустые
+                .body("message", equalTo("Недостаточно данных для создания учетной записи"));
+    }
 
     @AfterEach
     public void deleteCourier() {
         if (courierId != 0) {
             given()
-                    .baseUri(baseUri)
                     .header("Content-Type", "application/json")
                     .when()
                     .delete("api/v1/courier/" + courierId)
                     .then()
                     .statusCode(200)
                     .body("ok", equalTo(true));
+
+            courierId = 0;
         }
     }
-    @Test
-    @Description("Проверка создания курьера")
-    public void testCreateCourier() {
-        // Получаем текущую дату в формате yyyyMMdd
-        String currentDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
-
-        // Создаем уникальное имя курьера с добавлением текущей даты
-        String courierLogin = "login" + currentDate;
-
-        // Данные для создания курьера
-        String courierJson = "{ \"login\": \"" + courierLogin + "\", \"password\": \"12345\", \"firstName\": \"TestUser\" }";
-
-        // Создаем курьера
-        ValidatableResponse createResponse = given()
-                .baseUri(baseUri)
-                .header("Content-Type", "application/json")
-                .body(courierJson)
-                .when()
-                .post("api/v1/courier")
-                .then()
-                .statusCode(201)
-                .body("ok", equalTo(true));
-
-        courierId = createResponse.extract().path("id");
-    }
-
-//    @Test
-//    @Description("Проверка удаления курьера")
-//    public void testDeleteCourier() {
-//        // Проверяем, что курьер был создан и у нас есть ID для удаления
-//        if (courierId == 0) {
-//            throw new RuntimeException("Courier ID not found. Please create a courier first.");
-//        }
-//
-//        // Удаляем курьера
-//        given()
-//                .baseUri(baseUri)
-//                .header("Content-Type", "application/json")
-//                .when()
-//                .delete("api/v1/courier/" + courierId)
-//                .then()
-//                .statusCode(200)
-//                .body("ok", equalTo(true));
-//    }
-
-    @Test
-    @Description("Проверка создания двух одинаковых курьеров")
-    public void testCreateDuplicateCourier() {
-        String courierJson = "{ \"login\": \"duplicateLogin\", \"password\": \"12345\", \"firstName\": \"Test\" }";
-
-        given()
-                .baseUri(baseUri)
-                .header("Content-Type", "application/json")
-                .body(courierJson)
-                .when()
-                .post("api/v1/courier")
-                .then()
-                .statusCode(201)
-                .body("ok", equalTo(true));
-
-        given()
-                .baseUri(baseUri)
-                .header("Content-Type", "application/json")
-                .body(courierJson)
-                .when()
-                .post("api/v1/courier")
-                .then()
-                .statusCode(409)
-                .body("message", equalTo("Этот логин уже используется"));
-    }
-
-    @Test
-    @Description("Проверка создания курьера без обязательных полей")
-    public void testCreateCourierWithoutRequiredFields() {
-        String courierJson = "{ \"login\": \"\", \"password\": \"\", \"firstName\": \"\" }";
-
-        given()
-                .baseUri(baseUri)
-                .header("Content-Type", "application/json")
-                .body(courierJson)
-                .when()
-                .post("api/v1/courier")
-                .then()
-                .statusCode(400)
-                .body("message", equalTo("Недостаточно данных для создания курьера"));
-    }
-
-    @Test
-    @Description("Проверка логина курьера")
-    public void testLoginCourier() {
-        String loginJson = "{ \"login\": \"newLogin\", \"password\": \"12345\" }";
-
-        given()
-                .baseUri(baseUri)
-                .header("Content-Type", "application/json")
-                .body(loginJson)
-                .when()
-                .post("api/v1/courier/login")
-                .then()
-                .statusCode(200)
-                .body("id", notNullValue());
-    }
-
 }
--- src/test/java/ru/yandex/praktikum/OrderTest.java
+++ src/test/java/ru/yandex/praktikum/OrderTest.java
@@ -1,2 +1,40 @@
-package ru.yandex.praktikum;public class OrderTest {
+package ru.yandex.praktikum;
+
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class OrderTest {
+    private final String baseUri = "https://qa-scooter.praktikum-services.ru/";
+
+    @Test
+    @Description("Проверка создания заказа с параметризацией цветов")
+    public void testCreateOrder() {
+        String orderJson = "{ \"color\": [\"BLACK\"] }"; // Для одного цвета
+        given()
+                .baseUri(baseUri)
+                .header("Content-Type", "application/json")
+                .body(orderJson)
+                .when()
+                .post("api/v1/orders")
+                .then()
+                .statusCode(201)
+                .body("track", notNullValue());
+    }
+
+    @Test
+    @Description("Проверка получения списка заказов")
+    public void testGetOrders() {
+        given()
+                .baseUri(baseUri)
+                .header("Content-Type", "application/json")
+                .when()
+                .get("api/v1/orders")
+                .then()
+                .statusCode(200)
+                .body("orders", not(empty()));
+    }
+
+
 }
