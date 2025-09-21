558645c3b51a761c28ba1ea8ac14e30a9449c118
ZhannaDan <zh.i.danyaeva@gmail.com>
2025-09-21 16:09:01 +0300

    first commit

---
+++ src/test/java/RestAssuredGetAllureTest.java
@@ -0,0 +1,47 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class RestAssuredGetAllureTest {
+
+    String bearerToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODhlM2NjNTVkMGRmNzAwM2RkNjdjNjYiLCJpYXQiOjE3NTg0NTQzNDgsImV4cCI6MTc1OTA1OTE0OH0.flx8V1ZAQBdf9ca_Oy_lXD-7-54jh3U-9qn_Lxn5SVg";
+
+    @BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-mesto.praktikum-services.ru";
+    }
+
+    @Test
+    public void getMyInfoStatusCode() {
+        given()
+                .auth().oauth2(bearerToken)
+                .get("/api/users/me")
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void checkUserName() {
+        given()
+                .auth().oauth2(bearerToken)
+                .get("/api/users/me")
+                .then().assertThat().body("data.name",equalTo("Василий Васильев"));
+    }
+
+    @Test
+    public void checkUserNameAndPrintResponseBody() {
+
+        Response response =given().auth().oauth2(bearerToken).get("/api/users/me");
+        // отправили запрос и сохранили ответ в переменную response - экземпляр класса Response
+
+        response.then().assertThat().body("data.name",equalTo("Василий Васильев"));
+        // проверили, что в теле ответа ключу name соответствует нужное имя пользователя
+
+        System.out.println(response.body().asString()); // вывели тело ответа на экран
+
+    }
+
+}
