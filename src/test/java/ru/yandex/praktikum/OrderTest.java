ce6b761a90a3eea077af37b04a890ba7d2ee5f6a
ZhannaDan <zh.i.danyaeva@gmail.com>
2025-09-27 08:55:14 +0300

    fix теста с параметризацией

--- src/test/java/ru/yandex/praktikum/OrderTest.java
+++ src/test/java/ru/yandex/praktikum/OrderTest.java
@@ -2,22 +2,38 @@ package ru.yandex.praktikum;
 
 import io.qameta.allure.Description;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class OrderTest {
     private final String baseUri = "https://qa-scooter.praktikum-services.ru/";
 
-    @Test
-    @Description("Проверка создания заказа с параметризацией цветов")
-    public void testCreateOrder() {
-        String orderJson = "{ \"color\": [\"BLACK\"] }"; // Для одного цвета
+    @ParameterizedTest(name = "Создание заказа с цветом: {0}")
+    @ValueSource(strings = {"BLACK", "GREY", "BLACK,GREY"})
+    @Description("Проверка создания заказа с параметризацией по цветам")
+    public void testCreateOrderWithColors(String colors) {
+
+        String[] colorArray = colors.split(",");
+        StringBuilder colorJsonArray = new StringBuilder("[");
+        for (int i = 0; i < colorArray.length; i++) {
+            colorJsonArray.append("\"").append(colorArray[i].trim()).append("\"");
+            if (i < colorArray.length - 1) {
+                colorJsonArray.append(",");
+            }
+        }
+        colorJsonArray.append("]");
+
+        String orderJson = "{ \"color\": " + colorJsonArray.toString() + " }";
+
         given()
                 .baseUri(baseUri)
                 .header("Content-Type", "application/json")
                 .body(orderJson)
                 .when()
-                .post("api/v1/orders")
+                .post("/api/v1/orders")
                 .then()
                 .statusCode(201)
                 .body("track", notNullValue());
